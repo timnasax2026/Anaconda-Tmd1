bd0c4a0e3e9f7367139a051f8e3729592d40b8f9
Fredi AI <frediezra60@gmail.com>
2026-04-12 08:16:27 +0300

    Create Groups/close.js

---
+++ plugins/Groups/close.js
@@ -0,0 +1,10 @@
+const middleware = require('../../utility/botUtil/middleware');
+
+module.exports = async (context) => {
+    await middleware(context, async () => {
+        const { client, m } = context;
+
+        await client.groupSettingUpdate(m.chat, 'announcement');
+                m.reply('╭━━━━━━━━━━━━━━━━━━\n❒ Group closed.\n╰━━━━━━━━━━━━━━━━━━');
+    });
+};
